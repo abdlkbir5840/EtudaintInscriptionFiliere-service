52bf3bd540f1d090d0eb12bc7251c6c2f62024fc
Fatima-Ezzahra MAJDOU <tima@MacBook-Pro.local>
2023-04-27 13:48:09 +0100

    etudiant repository

---
+++ src/main/java/com/example/etudaintinscriptionfiliereservice/repositories/EtudiantRepository.java
@@ -0,0 +1,7 @@
+package com.example.etudaintinscriptionfiliereservice.repositories;
+
+import com.example.etudaintinscriptionfiliereservice.entities.Etudiant;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EtudiantRepository extends JpaRepository<Etudiant, Long> {
+}
