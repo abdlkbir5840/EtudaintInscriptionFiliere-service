e45791d663ec221fd36ee185047bb42b0c58fb2b
abdlkbir5840 <habonbahmadi@gmail.com>
2023-04-27 19:02:33 +0100

    j'ai fini filiere

--- src/main/java/com/example/etudaintinscriptionfiliereservice/entities/Filiere.java
+++ src/main/java/com/example/etudaintinscriptionfiliereservice/entities/Filiere.java
@@ -1,10 +1,12 @@
 package com.example.etudaintinscriptionfiliereservice.entities;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
 import javax.validation.constraints.NotBlank;
@@ -20,6 +22,7 @@ public class Filiere {
     @NotBlank(message = "name should not be empty")
     private String name;
     private Niveau niveau;
-    @OneToMany(mappedBy = "filiere")
+    @OneToMany(mappedBy = "filiere",fetch = FetchType.LAZY)
+    @JsonIgnore
     private List<Inscription> inscriptions;
 }
